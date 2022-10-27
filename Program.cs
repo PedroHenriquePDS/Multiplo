028d60cfd54d2efd7f1842ceb1033beb3f72742f
Pedro Henrique <Pedrohenrique053010@gmail.com>
2022-10-27 17:48:19 -0300

    Melhora a solução

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿System.Console.WriteLine("Digite um número...: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+System.Console.WriteLine("Avaliar se é múltiplo de...: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (n1 % n2 == 0)
+{
+
+    System.Console.WriteLine($"\n{n1} é múltiplo de {n2}");
+}
+
+else
+{
+
+    System.Console.WriteLine($"\n{n1} não é múltiplo de {n2}");
+}
