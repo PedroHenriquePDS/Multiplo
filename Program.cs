5ca16149a455dc6c808a602c0ca0492007b10a9a
Pedro Henrique <PedroHenrique053010@gmail.com>
2022-11-04 22:13:36 -0300

    melhora a resolução

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿System.Console.WriteLine("Digite um número...: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-
-
-
-System.Console.WriteLine("Avaliar se é múltiplo de...: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-
-if (n1 % n2 == 0)
+﻿System.Console.WriteLine("--Multiplo--\n");
+System.Console.Write("Digite um número:        ");
+int numero = Convert.ToInt32(Console.ReadLine()!);
+System.Console.Write("Avalie se é múltiplo de: ");
+int multiplo = Convert.ToInt32(Console.ReadLine()!);
+if (numero % multiplo == 0)
 {
-
-    System.Console.WriteLine($"\n{n1} é múltiplo de {n2}");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    System.Console.WriteLine($"{numero} é multiplo de {multiplo}\n");
+    Console.ResetColor();
 }
-
 else
 {
-
-    System.Console.WriteLine($"\n{n1} não é múltiplo de {n2}");
+    Console.ForegroundColor = ConsoleColor.Red;
+    System.Console.WriteLine($"{numero} não é múltiplo de {multiplo}\n");
+    Console.ResetColor();
+System.Console.Write("Pressione uma tecla para fechar...");
+Console.ReadKey();
 }
+
